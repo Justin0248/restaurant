45ef05972ae6eeddfeac20d3400f7d8743e25af2
Justin0248 <justin12689@gmail.com>
2024-04-06 16:41:19 -0700

    home page setup

---
+++ backend/menuItem.js
@@ -0,0 +1,112 @@
+const menuItems = [
+  {
+    id: 8,
+    name: 'Chicken Sandwich',
+    price: 4.99,
+    category: 'Main'
+  },
+  {
+    id: 9,
+    name: 'Spaghetti Bolognese',
+    price: 7.99,
+    category: 'Main'
+  },
+  {
+    id: 10,
+    name: 'Margherita Pizza',
+    price: 9.99,
+    category: 'Main'
+  },
+  {
+    id: 11,
+    name: 'Hamburger',
+    price: 5.99,
+    category: 'Main'
+  },
+  {
+    id: 12,
+    name: 'Cheeseburger',
+    price: 6.49,
+    category: 'Main'
+  },
+  {
+    id: 13,
+    name: 'Veggie Burger',
+    price: 5.49,
+    category: 'Main'
+  },
+  {
+    id: 14,
+    name: 'Chicken Wings (8 pcs)',
+    price: 6.99,
+    category: 'Appetizers'
+  },
+  {
+    id: 15,
+    name: 'Nachos',
+    price: 5.99,
+    category: 'Appetizers'
+  },
+  {
+    id: 16,
+    name: 'Cheese Platter',
+    price: 8.99,
+    category: 'Appetizers'
+  },
+  {
+    id: 17,
+    name: 'Onion Rings',
+    price: 3.99,
+    category: 'Appetizers'
+  },
+  {
+    id: 18,
+    name: 'Garlic Bread',
+    price: 3.49,
+    category: 'Appetizers'
+  },
+  {
+    id: 19,
+    name: 'Mozzarella Sticks',
+    price: 4.99,
+    category: 'Appetizers'
+  },
+  {
+    id: 20,
+    name: 'Soda (Large)',
+    price: 2.49,
+    category: 'Drinks'
+  },
+  {
+    id: 21,
+    name: 'Mango Lassi',
+    price: 3.49,
+    category: 'Drinks'
+  },
+  {
+    id: 22,
+    name: 'Iced Coffee',
+    price: 4.99,
+    category: 'Drinks'
+  },
+  {
+    id: 23,
+    name: 'Lemonade',
+    price: 2.99,
+    category: 'Drinks'
+  },
+  {
+    id: 24,
+    name: 'Hot Chocolate',
+    price: 3.99,
+    category: 'Drinks'
+  },
+  {
+    id: 25,
+    name: 'Fruit Punch',
+    price: 3.49,
+    category: 'Drinks'
+  }
+];
+
+module.exports = menuItems;
